55e26b4cecdaa72c07498fccd7e26f5fdae1fdad
Muhammad Osman <mmuhammadusman1@gmail.com>
2021-01-31 01:12:34 +0500

    Create test_db.php

---
+++ test_db.php
@@ -0,0 +1,23 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "nts";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "select table_schema,0x3a,table_name,0x3a,column_name from information_schema.columns";
+
+$result = mysqli_query($conn, $sql);
+
+while($result = mysqli_fetch_array($query))
+		{
+			print_r($result);
+		}
+
+
+?>
